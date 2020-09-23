f9b410eb03a7982c79b813dffabadf59b2bc2f0c
jsw7524 <jsw7524@gmail.com>
2020-09-24 01:50:51 +0800

    Simple SudokuUI

--- Sudoku/helper.cs
+++ Sudoku/helper.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Sudoku
 {
@@ -21,5 +22,17 @@ namespace Sudoku
             var json = JsonConvert.SerializeObject(data);
             File.WriteAllText(filename, json);
         }
+
+        public static string GetBoardJson(SudokuBoard board)
+        {
+            List<List<int>> tmp = new List<List<int>>();
+
+            for (int i=0;i<9;i++)
+            {
+                tmp.Add(board.GetRow(i).Select(c=>c.Value).ToList());
+            }
+            return "{\"board\":" + JsonConvert.SerializeObject(tmp) + "}";
+        }
+
     }
 }
---
+++ SudokuUI/Controllers/SudokuController.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using Sudoku;
+using SudokuUI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SudokuUI.Controllers
+{
+    public class SudokuController : Controller
+    {
+        public ActionResult GetSudokuBoardData()
+        {
+            string json = null;
+            using (var client = new HttpClient())
+            {
+                //HTTP GET
+                var responseTask = client.GetAsync(new Uri("https://sugoku.herokuapp.com/board?difficulty=easy"));
+                responseTask.Wait();
+                var result = responseTask.Result;
+
+                var readTask = result.Content.ReadAsStringAsync();
+                readTask.Wait();
+
+
+                json = readTask.Result;
+            }
+            return Json(json, JsonRequestBehavior.AllowGet);
+        }
+
+        int[,] DicToIntArray(Dictionary<string, object> dataJson)
+        {
+            var tmp = dataJson.Select(r => r.Value as string[]).ToList();
+            int[,] result = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    result[i, j] = int.Parse((tmp[i])[j]);
+                }
+            }
+            return result;
+
+        }
+
+        [HttpPost]
+        public ActionResult sovleSudoku(Dictionary<string, object> dataJson)
+        {
+            int[,] data = DicToIntArray(dataJson);
+            SudokuBoard board = new SudokuBoard(data);
+            SudokuSolver solver = new SudokuSolver();
+            solver.BestFirstSearch(board);
+            var result = helper.GetBoardJson(board);
+
+            return Json(result);
+        }
+
+        // GET: Sudoku
+        public ActionResult Index()
+        {
+            SudokuViewModel viewModel = new SudokuViewModel();
+
+            return View(viewModel);
+        }
+    }
+}
